859b2292945699ceb42da9981458caeec2318f8f
its-kaps <44054605+its-kaps@users.noreply.github.com>
2021-09-18 16:09:30 +0530

    Add files via upload

---
+++ FurnitureFactory/src/main/java/hackerrank/Furniture.java
@@ -0,0 +1,47 @@
+package hackerrank;
+
+/**
+ * FURNITURE_TYPE("Furniture Name", floating-point cost)
+*/
+
+
+public enum Furniture {
+	
+	CHAIR("Chair", 100.0f),
+	TABLE("Table", 250.0f),
+	COUCH("Couch", 500.0f);
+
+	private final String label;
+	private final float cost;
+
+	/**
+	 * @param label The plain text name of the furniture
+	 * @param cost The furniture's cost
+	 */
+	Furniture(String label, float cost) {
+		this.label = label;
+		this.cost = cost;
+	}
+
+	/**
+	 * @return The plain text name of the furniture
+	 */
+	public String label() {
+		return label;
+	}
+
+	/**
+	 * @return The furniture's cost
+	 */
+	public float cost() {
+		if(label == "Chair") {
+			return 100.0f;
+		}else if(label == "Table") {
+			return 250.0f;
+		}else if(label == "Couch") {
+			return 500.0f;
+		}else {
+			return -0.1f;
+		}
+	}
+}
